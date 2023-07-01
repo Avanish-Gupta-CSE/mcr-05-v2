3c9bc729e699cf3c88617294029c482c96510349
Avanish Kumar Gupta <91612116+Avanish-Gupta-CSE@users.noreply.github.com>
2023-07-01 21:44:28 +0530

    feat: basic features done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { BrowserRouter as Router } from "react-router-dom";
+import AppContextProvider from "./context/AppContextProvider";
+import theme from "./theme";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <Router>
+    <AppContextProvider>
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </AppContextProvider>
+  </Router>
+  // </React.StrictMode>
 );
-
